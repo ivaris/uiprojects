a46df945479dbc9f4529e0c48df5db3b5f14f3bc
Ravi <ivar.is@outlook.com>
2017-01-17 23:43:53 -0500

    added new component

--- myAngular2/src/app/app.component.ts
+++ myAngular2/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {CoursesComponent} from './courses.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works by Ravi!';
+  modules = ['module1', 'module2'];
 }
